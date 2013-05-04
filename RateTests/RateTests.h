86cf73e69e1b8f6f39a068fe75bd69881586f379
administrator <std@STU23.local>
2013-05-04 12:57:55 +0700

    Initial Commit

---
+++ RateTests/RateTests.h
@@ -0,0 +1,13 @@
+//
+//  RateTests.h
+//  RateTests
+//
+//  Created by administrator on 5/4/56 BE.
+//  Copyright (c) 2556 siriphan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RateTests : SenTestCase
+
+@end
---
+++ RateTests/RateTests.m
@@ -0,0 +1,32 @@
+//
+//  RateTests.m
+//  RateTests
+//
+//  Created by administrator on 5/4/56 BE.
+//  Copyright (c) 2556 siriphan. All rights reserved.
+//
+
+#import "RateTests.h"
+
+@implementation RateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RateTests");
+}
+
+@end
